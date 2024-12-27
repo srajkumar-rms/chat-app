c7382f3e9b9093603302fad1e5fc03f6864a0bd9
Rajkumar Satish (Moody's) <satishr1@moodys.com>
2024-12-27 15:53:03 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import { Server } from 'socket.io'
+import http from 'http'
+import cors from 'cors'
+
+const app = express()
+
+//1. create server using http
+const server = http.createServer(app)
+
+//2. create socket server
+const io = new Server(server,{
+    cors: {
+        origin: '*',
+        methods:["GET","POST"]
+    }
+});
+
+//3. User socket events.
+io.on('connection',(socket)=>{
+    console.log("Connection is established");
+    
+    socket.on('new_message', (message)=>{
+        //broadcast this message to all clients
+        socket.broadcast.emit('broadcast_message',message)
+
+    })
+    socket.on('disconnect',()=>{
+        console.log('Connection is disconnected');
+        
+    })
+})
+
+
+server.listen(3000,()=>{
+    console.log('App is listinging on 3000');
+    
+})
